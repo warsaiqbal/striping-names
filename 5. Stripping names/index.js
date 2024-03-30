43d47b4361cfb77f3698c9d65596e85a65908e20
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 06:45:02 -0700

    Add files via upload

---
+++ 5. Stripping names/index.js
@@ -0,0 +1,4 @@
+var name_with_whitespace = "\t \n Allama Iqbal \t \n";
+console.log(name_with_whitespace);
+var onlyName = "Allama Iqbal";
+console.log(onlyName);
---
+++ 5. Stripping names/index.ts
@@ -0,0 +1,6 @@
+let name_with_whitespace: string = "\t \n Allama Iqbal \t \n"
+console.log(name_with_whitespace);
+
+let onlyName: string = "Allama Iqbal"
+console.log(onlyName);
+
